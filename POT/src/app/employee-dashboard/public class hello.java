3b2f9e350857b94cbf41132feef2f4f46029ae88
AswanthkumarMuthineni <144228416+AswanthkumarMuthineni@users.noreply.github.com>
2024-09-17 10:31:20 +0530

    extension

---
+++ POT/src/app/employee-dashboard/public class hello.java
@@ -0,0 +1,11 @@
+public class hello
+{
+    public static void main(String args())
+    {
+       int s1=1;
+       int s3=2;
+       int s4;
+       s4=s1+s3;
+       System.out.println(s4); 
+    }
+}
